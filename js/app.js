8779ffef80804ada54d703e23a8e0f828199a600
kanan132 <kenan.132@mail.ru>
2020-08-13 00:45:07 +0400

    updated

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function () { myFunction() };
 
 var navbar = document.querySelector(".navbar");
 var sticky = navbar.offsetTop;
@@ -10,3 +10,62 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+document.getElementById("student-info").innerHTML = `<div class="col-md-12 resume">
+                                <div class="tabbable resume1" id="tabs-928477">
+                                  <ul class="nav nav-tabs">
+                                    <li class="nav-item">
+                                      <a class="nav-link active show" href="#tab1" data-toggle="tab" style="background-color:#061A40">Resume</a>
+                                    </li>
+                                    
+                                  </ul>
+                                  <div class="tab-content">
+                                    <div class="tab-pane active" id="panel-231828">
+                                    <img src="img/background_result.jpg" width="100%"
+                                    height="250">
+                                    <div class="profile-img">
+                                      <img src="img/profile-photo.webp" width="140" height="140">
+                                    </div>  
+                                    <div class="centered">
+                                      <h3 class="first-title">Kanan Gafarov</h3>
+                                      
+                                    </div>
+                                  </div>
+                                    
+                                  </div>
+                                </div>
+                                <div id="card-603561" class="resume2">
+                                  <div class="card">
+                                    <div class="card-header">
+                                      <a class="card-link collapsed" data-toggle="collapse" data-parent="#card-603561" href="#card-element-420134">More information</a>
+                                    </div>
+                                    <div id="card-element-420134" class="collapse">
+                                      <div class="card-body">
+                                      <div class="first">
+                                        <h3>Skills:</h3>
+                                        <ul>
+                                          <li>
+                                          Exceptional communication and networking skill
+                                          </li>
+                                          <li>
+                                          The ability to work under pressure and multi-task
+                                          </li>
+                                        </ul>
+                                      </div>
+                                      <div class="second">
+                                        <h3>Education</h3>
+                                        <ul>
+                                          <li>
+                                          Petroleum enginneering
+                                          </li>
+                                          <li>
+                                          Full stack python(tech academy)
+                                          </li>
+                                        </ul>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                  
+                                </div>
+                                </div>`
